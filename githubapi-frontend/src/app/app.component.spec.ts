c2ad908c27fca7d8f3bf97143de3d978787cb11e
ketryn <ketryn29@gmail.com>
2022-08-03 22:45:25 -0300

    teste

--- githubapi-frontend/src/app/app.component.spec.ts
+++ githubapi-frontend/src/app/app.component.spec.ts
@@ -28,12 +28,4 @@ describe('AppComponent', () => {
     expect(app.title).toEqual('githubapi-frontend');
   });
 
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain(
-      'githubapi-frontend app is running!'
-    );
-  });
 });
