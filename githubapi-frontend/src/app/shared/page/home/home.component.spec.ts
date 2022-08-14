03d8ed71070caa9be79d9e1d441335a29e0310cc
ketryn <ketryn29@gmail.com>
2022-08-14 15:42:54 -0300

    build

--- githubapi-frontend/src/app/shared/page/home/home.component.spec.ts
+++ githubapi-frontend/src/app/shared/page/home/home.component.spec.ts
@@ -1,3 +1,4 @@
+import { HttpClientModule } from '@angular/common/http';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { HomeComponent } from './home.component';
@@ -9,6 +10,7 @@ describe('HomeComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [HomeComponent],
+      imports: [HttpClientModule]
     }).compileComponents();
 
     fixture = TestBed.createComponent(HomeComponent);
