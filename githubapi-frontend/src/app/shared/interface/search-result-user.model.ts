3f41f5bd79a693dd68ba4b974ecd98af11d5c65e
ketryn <ketryn29@gmail.com>
2022-08-17 17:32:23 -0300

    ajuste

--- githubapi-frontend/src/app/shared/interface/search-result-user.model.ts
+++ githubapi-frontend/src/app/shared/interface/search-result-user.model.ts
@@ -1,7 +1,6 @@
 import { User } from "./user.model";
 
 export interface SearchResultUser {
-  profile: import("c:/Users/ketryn.miranda/Documents/GitHub/desafio-githubapi-frontend/githubapi-frontend/src/app/shared/interface/user-result.model").UserResult[];
   total_count: number;
   incomplete_results: boolean,
   items: User[],
