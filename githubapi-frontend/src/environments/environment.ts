aa6d65cafb007f24617221cd3bb6b3e6c9536e71
ketryn <ketryn29@gmail.com>
2022-08-18 16:02:14 -0300

    ajustando os codigos e os erros no test

--- githubapi-frontend/src/environments/environment.ts
+++ githubapi-frontend/src/environments/environment.ts
@@ -3,7 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false,
+  production: true,
   API: 'https://api.github.com',
   token: `token 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`
 };
