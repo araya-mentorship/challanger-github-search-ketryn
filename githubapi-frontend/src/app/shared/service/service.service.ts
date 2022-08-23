04e4e0936e6dc99e66832a6505f35201de2b5882
ketryn <ketryn29@gmail.com>
2022-08-23 12:24:54 -0300

    config console

--- githubapi-frontend/src/app/shared/service/service.service.ts
+++ githubapi-frontend/src/app/shared/service/service.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import { environment } from 'src/environments/environment';
+import { environment } from '../../../environments/environment';
 import { User } from '../interface/user.model';
 import { SearchResultUser } from '../interface/search-result-user.model';
 import { UserResult } from '../interface/user-result.model';
