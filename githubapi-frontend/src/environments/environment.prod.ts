b6e9783837275a34917299fd8dcf4585a20451de
eduardoaraya <eduardojezine@gmail.com>
2022-08-24 11:18:52 -0300

    [feature]: Ajustando falha na build

--- githubapi-frontend/src/environments/environment.prod.ts
+++
@@ -1,8 +0,0 @@
-import * as dotenv from 'dotenv' 
-
-dotenv.config()
-export const environment = {
-  production: true,
-  API: process.env['API'],
-  token: process.env['TOKEN']
-};
--- githubapi-frontend/src/environments/environment.ts
+++
@@ -1,6 +0,0 @@
-// Arquivo gerado automaticamente pelo script /tools/loadenv.js
-export const environment = {
-    production: false,
-    API: "https://api.github.com",
-    token: "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",
-};
